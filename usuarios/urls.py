a7ae746004ddb6b9138d56b45a3a01b97d419769
Esteban S. <estebansambachi@gmail.com>
2025-07-11 17:59:29 -0500

    Agregue plantillas de panel de administración para cursos y estudiantes, incluida la navegación en la barra lateral y modales para administrar profesores y estudiantes.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,8 +4,11 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),  # usa tu vista personalizada
     path('panel/admin/', views.admin_dashboard, name='admin_dashboard'),
     path('panel/admin/profesores/', views.admin_profesores, name='admin_profesores'),
+    path('panel/admin/estudiantes/', views.admin_estudiantes, name='admin_estudiantes'),
+    path('panel/admin/cursos/', views.admin_cursos, name='admin_cursos'),
     path('panel/profesor/', views.profesor_dashboard, name='profesor_dashboard'),
     path('panel/estudiante/', views.estudiante_dashboard, name='estudiante_dashboard'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- usuarios/views.py
+++ usuarios/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Usuario
 from django.db import IntegrityError
+from django.contrib.auth import logout
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -25,11 +27,18 @@ def login_view(request):
 
     return render(request, 'usuarios/login.html')
 
+def logout_view(request):
+    list(messages.get_messages(request))  # Esto borra los mensajes pendientes
+    logout(request)
+    return redirect('login')
+
 @login_required
 def admin_dashboard(request):
     if request.user.rol != Usuario.Rol.ADMIN:
         return redirect('login')  # o a otra página de acceso denegado
-    return render(request, 'usuarios/admin_dashboard.html')
+    return render(request, 'usuarios/admin_dashboard.html', {
+        'active_tab': 'inicio'
+    })
 
 @login_required
 def profesor_dashboard(request):
@@ -44,39 +53,146 @@ def admin_profesores(request):
     if request.user.rol != Usuario.Rol.ADMIN:
         return redirect('login')
     if request.method == 'POST':
-        cedula = request.POST.get('cedula', '')
-        first_name = request.POST.get('first_name', '')
-        last_name = request.POST.get('last_name', '')
-        email = request.POST.get('email', '')
-        instrumento = request.POST.get('instrumento', '')
-        telefono = request.POST.get('telefono', '')
-        direccion = request.POST.get('direccion', '')
-        password = request.POST.get('password', '')
-        confirm_password = request.POST.get('confirm_password', '')
-        rol = Usuario.Rol.PROFESOR
-
-        if password != confirm_password:
-            messages.error(request, 'Las contraseñas no coinciden.')
-        elif Usuario.objects.filter(email=email).exists():
-            messages.error(request, 'El email ya está registrado.')
-        else:
+        profesor_id = request.POST.get('profesor_id')
+        # Eliminar profesor si el form de eliminación fue enviado
+        if request.POST.get('eliminar_profesor') == '1' and profesor_id:
             try:
-                usuario = Usuario.objects.create_user(
-                    username=email,
-                    email=email,
-                    password=password,
-                    first_name=first_name,
-                    last_name=last_name,
-                    rol=rol,
-                    cedula=cedula,
-                    telefono=telefono,
-                    direccion=direccion,
-                    instrumento=instrumento,
-                    is_active=True
-                )
-                messages.success(request, 'Profesor registrado exitosamente.')
-                return redirect('admin_profesores')
-            except IntegrityError:
-                messages.error(request, 'Error al registrar el profesor. Intente con otro email.')
+                profesor = Usuario.objects.get(id=profesor_id, rol=Usuario.Rol.PROFESOR)
+                profesor.delete()
+                messages.success(request, 'Profesor eliminado exitosamente.')
+            except Usuario.DoesNotExist:
+                messages.error(request, 'Profesor no encontrado.')
+            return redirect('admin_profesores')
+        elif profesor_id:
+            # Edición de profesor
+            try:
+                profesor = Usuario.objects.get(id=profesor_id, rol=Usuario.Rol.PROFESOR)
+                profesor.cedula = request.POST.get('cedula', '')
+                profesor.first_name = request.POST.get('first_name', '')
+                profesor.last_name = request.POST.get('last_name', '')
+                profesor.email = request.POST.get('email', '')
+                profesor.username = request.POST.get('email', '')
+                profesor.instrumento = request.POST.get('instrumento', '')
+                profesor.telefono = request.POST.get('telefono', '')
+                profesor.direccion = request.POST.get('direccion', '')
+                profesor.save()
+                messages.success(request, 'Profesor actualizado exitosamente.')
+            except Usuario.DoesNotExist:
+                messages.error(request, 'Profesor no encontrado.')
+            return redirect('admin_profesores')
+        else:
+            # Alta de profesor
+            cedula = request.POST.get('cedula', '')
+            first_name = request.POST.get('first_name', '')
+            last_name = request.POST.get('last_name', '')
+            email = request.POST.get('email', '')
+            instrumento = request.POST.get('instrumento', '')
+            telefono = request.POST.get('telefono', '')
+            direccion = request.POST.get('direccion', '')
+            password = request.POST.get('password', '')
+            confirm_password = request.POST.get('confirm_password', '')
+            rol = Usuario.Rol.PROFESOR
+
+            if password != confirm_password:
+                messages.error(request, 'Las contraseñas no coinciden.')
+            elif Usuario.objects.filter(email=email).exists():
+                messages.error(request, 'El email ya está registrado.')
+            else:
+                try:
+                    usuario = Usuario.objects.create_user(
+                        username=email,
+                        email=email,
+                        password=password,
+                        first_name=first_name,
+                        last_name=last_name,
+                        rol=rol,
+                        cedula=cedula,
+                        telefono=telefono,
+                        direccion=direccion,
+                        instrumento=instrumento,
+                        is_active=True
+                    )
+                    messages.success(request, 'Profesor registrado exitosamente.')
+                    return redirect('admin_profesores')
+                except IntegrityError:
+                    messages.error(request, 'Error al registrar el profesor. Intente con otro email.')
     profesores = Usuario.objects.filter(rol=Usuario.Rol.PROFESOR)
-    return render(request, 'usuarios/admin_profesores.html', {'profesores': profesores})
+    return render(request, 'usuarios/admin_profesores.html', {'profesores': profesores ,
+    'active_tab': 'profesores'})
+
+@login_required
+def admin_cursos(request):
+    return render(request, 'usuarios/admin_cursos.html', {
+        'active_tab': 'cursos'
+    })
+
+@login_required
+def admin_estudiantes(request):
+    if request.user.rol != Usuario.Rol.ADMIN:
+        return redirect('login')
+    if request.method == 'POST':
+        estudiante_id = request.POST.get('estudiante_id')
+        # Eliminar estudiante si el form de eliminación fue enviado
+        if request.POST.get('eliminar_estudiante') == '1' and estudiante_id:
+            try:
+                estudiante = Usuario.objects.get(id=estudiante_id, rol=Usuario.Rol.ESTUDIANTE)
+                estudiante.delete()
+                messages.success(request, 'Estudiante eliminado exitosamente.')
+            except Usuario.DoesNotExist:
+                messages.error(request, 'Estudiante no encontrado.')
+            return redirect('admin_estudiantes')
+        elif estudiante_id:
+            # Edición de estudiante
+            try:
+                estudiante = Usuario.objects.get(id=estudiante_id, rol=Usuario.Rol.ESTUDIANTE)
+                estudiante.cedula = request.POST.get('cedula', '')
+                estudiante.first_name = request.POST.get('first_name', '')
+                estudiante.last_name = request.POST.get('last_name', '')
+                estudiante.email = request.POST.get('email', '')
+                estudiante.username = request.POST.get('email', '')
+                estudiante.instrumento = request.POST.get('instrumento', '')
+                estudiante.telefono = request.POST.get('telefono', '')
+                estudiante.direccion = request.POST.get('direccion', '')
+                estudiante.save()
+                messages.success(request, 'Estudiante actualizado exitosamente.')
+            except Usuario.DoesNotExist:
+                messages.error(request, 'Estudiante no encontrado.')
+            return redirect('admin_estudiantes')
+        else:
+            # Alta de estudiante
+            cedula = request.POST.get('cedula', '')
+            first_name = request.POST.get('first_name', '')
+            last_name = request.POST.get('last_name', '')
+            email = request.POST.get('email', '')
+            instrumento = request.POST.get('instrumento', '')
+            telefono = request.POST.get('telefono', '')
+            direccion = request.POST.get('direccion', '')
+            password = request.POST.get('password', '')
+            confirm_password = request.POST.get('confirm_password', '')
+            rol = Usuario.Rol.ESTUDIANTE
+
+            if password != confirm_password:
+                messages.error(request, 'Las contraseñas no coinciden.')
+            elif Usuario.objects.filter(email=email).exists():
+                messages.error(request, 'El email ya está registrado.')
+            else:
+                try:
+                    usuario = Usuario.objects.create_user(
+                        username=email,
+                        email=email,
+                        password=password,
+                        first_name=first_name,
+                        last_name=last_name,
+                        rol=rol,
+                        cedula=cedula,
+                        telefono=telefono,
+                        direccion=direccion,
+                        instrumento=instrumento,
+                        is_active=True
+                    )
+                    messages.success(request, 'Estudiante registrado exitosamente.')
+                    return redirect('admin_estudiantes')
+                except IntegrityError:
+                    messages.error(request, 'Error al registrar el estudiante. Intente con otro email.')
+    estudiantes = Usuario.objects.filter(rol=Usuario.Rol.ESTUDIANTE)
+    return render(request, 'usuarios/admin_estudiantes.html', {'estudiantes': estudiantes, 'active_tab': 'estudiantes'})

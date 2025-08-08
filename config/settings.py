439f483892030d546d5c279c4e582153fe863bf3
MateoCh2001 <cevallostituanajulio@gmail.com>
2025-08-08 11:57:33 -0500

    subida desde laptop mateo

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'academia_db',
         'USER': 'root',
-        'PASSWORD': 'academiaMusical',
+        'PASSWORD': 'mateo123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
